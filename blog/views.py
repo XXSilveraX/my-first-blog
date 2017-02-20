9cd4384cea16fad48c4e37ffdc658dd42f46e3f5
XXSilveraX <XXSilveraX@users.noreply.github.com>
2017-02-20 17:52:48 +0800

    New data base, fourth patch

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,9 @@ from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts':posts})
         ## 'request' - everything received from the user via internet
         ## 'blog/post_list.html' - template file
         ## '{}' - items can be added here for use in template
         ## part before ':' is a string and needs ''
-
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':posts})
