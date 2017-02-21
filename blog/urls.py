d28aa07b106c5df7be67c4ed2f614f338baff7b2
XXSilveraX <XXSilveraX@users.noreply.github.com>
2017-02-21 15:19:05 +0800

    added views to create/edit blog post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list,name = 'post_list'), ## 'name' indicates the name of the URL to identify the view
     url(r'^post/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
